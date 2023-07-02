bf6226c71595c594289aa44f5bcf0f04aa377efa
NataliMax <maximovs@1876gmail.com>
2023-07-02 12:15:58 +0200

    newsolution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,43 +4,17 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     let result = '';
 
-    switch (this[0]) {
-      case null:
-        break;
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === (undefined)) {
+      } else {
+        result += this[i];
+      }
 
-      case undefined:
-        return result;
-
-      default:
-        result += this[0];
-    }
-
-    for (let i = 1; i < this.length; i++) {
-      switch (separator) {
-        case '':
-          result += this[i];
-          break;
-
-        case null:
-          result += 'null';
-          result += this[i];
-          break;
-
-        case (undefined):
-          result += ',';
-          result += this[i];
-          break;
-
-        default:
-          if (this[i] === null || this[i] === undefined) {
-            result += separator;
-            break;
-          }
-          result += separator;
-          result += this[i];
+      if (this[i] !== this[this.length - 1]) {
+        result += separator;
       }
     }
 
